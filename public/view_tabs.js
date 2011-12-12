3beafd6b81aac9308141c7c8a06c10989e82a1d3
uu59 <k@uu59.org>
2011-12-12 22:17:19 +0900

    refactoring

--- public/view_tabs.js
+++ public/view_tabs.js
@@ -24,6 +24,61 @@ View.Tabs = (function(){
     });
   }
 
+  function loadStreams() {
+    var d = $.getJSON('/streams', function(streams){
+      models = [];
+      streams.forEach(function(stream){
+        var st = new Stream(stream.id);
+        st.label = stream.label;
+        st.setMaxId(stream.max_id);
+        models.push(st);
+      });
+
+      $('#tabs #streams').html("");
+      models.forEach(function(model){
+        var bunch = new TweetsBunch(model);
+        $('#tabs ol#streams').append(bunch.element());
+      });
+    });
+    $('#tabs #streams').html(View.loadingIcon(d));
+    return d;
+  }
+
+  function loadTimelines() {
+    var d = $.getJSON('/timelines', function(tls){
+      models = [];
+      var order = Data.get('timelineOrder');
+      if(order){
+        tls.sort(function(a,b){
+          return order.indexOf(a.id) - order.indexOf(b.id);
+        });
+      }
+      tls.forEach(function(timeline){
+        var tl = new Timeline(timeline.id);
+        tl.label = timeline.label;
+        tl.setMaxId(Data.get(tl.wrapDataPrefix("maxid")) || 1);
+        tl.streams = [];
+        timeline.streams.forEach(function(st){
+          var stream = new Stream(st.id);
+          stream.label = st.label;
+          stream.timelines.push(tl);
+          stream.timelines = $.unique(stream.timelines);
+          tl.streams.push(stream);
+        });
+        tl.streams = $.unique(tl.streams);
+        models.push(tl);
+      });
+
+      $('#tabs #timelines').html('');
+      models.forEach(function(model){
+        var bunch = new TweetsBunch(model);
+        $('#tabs #timelines').append(bunch.element());
+      });
+    });
+    $('#tabs #timelines').html(View.loadingIcon(d));
+    return d;
+  }
+
   return {
     init: function(){
       var unreadStreams = {}, unreadTimelines = {};
@@ -37,8 +92,8 @@ View.Tabs = (function(){
       });
 
       return $.when(
-        View.Tabs.Streams.fetch(),
-        View.Tabs.Timelines.fetch()
+        loadStreams(),
+        loadTimelines()
       ).done(function(){
         init();
         $.each(unreadStreams, function(id, _){
@@ -48,9 +103,8 @@ View.Tabs = (function(){
         $.each(unreadTimelines, function(id, _){
           $('#tabs #timelines *[data-id="'+id+'"]').addClass('updated');
         });
-      }).done(function(){
       });
-    }, // init end
+    },
 
     check: function(){
       var streams = [], parallelLevel = 2;
